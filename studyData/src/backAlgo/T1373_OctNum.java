b574fa5b70ea5fb4c9c20457461a8a752796cf46
Shim.hw <shim.hw0810@gmail.com>
2017-03-15 20:03:20 +0900

    자료구조 학습-배열소스

---
+++ studyData/src/backAlgo/T1373_OctNum.java
@@ -0,0 +1,12 @@
+package backAlgo;
+
+import java.util.Scanner;
+
+public class T1373_OctNum {
+	public static void main(String[] args) {
+		try(Scanner scan = new Scanner(System.in)) {
+			StringBuffer buffer = new StringBuffer(scan.nextLine());
+			
+		}
+	}
+}
