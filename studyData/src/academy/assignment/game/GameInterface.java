835ed19f330aec737975041d0859dd3be537b53e
Shim.hw <shim.hw0810@gmail.com>
2017-04-11 00:03:11 +0900

    organize old source

---
+++ studyData/src/academy/assignment/game/GameInterface.java
@@ -0,0 +1,11 @@
+package academy.assignment.game;
+
+/**
+ * @author Shim Hyun-Woo
+ * @Created 2016.11
+ */
+public interface GameInterface {
+	public static void printResult(int myHand, int gameResult) {}
+	public static int getZeroToRange(int range) { return 0; }
+	public static void setIcon() {}
+}
