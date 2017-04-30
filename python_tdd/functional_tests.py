8e73d759c478241ddc71c29ec5ad1e3c1c929679
Shim.hw <shim.hw0810@gmail.com>
2017-04-30 10:47:41 +0900

    Add test to add task

--- python_tdd/functional_tests.py
+++ python_tdd/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -25,10 +26,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 작업을 추가
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            '작업 입력'
+        )
+
+        # "공구 사기"라고 텍스트박스에 입력
+        input_box.send_keys('공구 사기')
+
+        # Enter key 입력 시 페이지 갱신 및 작업목록에 '공구 사기' 추가됨
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공구 사기' for row in rows),
+        )
+
         self.fail('Finish the test') # 강제로 Test 실패 발생
 
-    # Test-story 에 맞추어 작성
-    # ...
+        # Test-story 에 맞추어 작성
+        # ...
 
 
 # import 가 아닌 Cmd 를 통함 unittest.main() 호출(Test Class 와 Method 를 찾아 실행)
