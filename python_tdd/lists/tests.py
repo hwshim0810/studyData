75e165bc67a00f252fb57c7c40a926425d81a627
Shim.hw <shim.hw0810@gmail.com>
2017-05-01 00:30:38 +0900

    Add model class & migrations

--- python_tdd/lists/tests.py
+++ python_tdd/lists/tests.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 from django.http import HttpRequest
 
 from lists.views import index
-from lists.models import Item
+from lists.models import Item, List
 
 
 class IndexPageTest(TestCase):
@@ -21,15 +21,23 @@ class IndexPageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -37,7 +45,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -45,9 +55,10 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-one/')
         self.assertTemplateUsed(response, 'lists/list.html')
 
-    def test_index_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    def test_displays_all_list_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-one/')
 
