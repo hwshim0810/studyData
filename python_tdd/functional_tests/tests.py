12fbec689d5c3b193cdbab13382be372a501e40f
Shim.hw <shim.hw0810@gmail.com>
2017-05-02 23:51:08 +0900

    Modified setting server url

--- python_tdd/functional_tests/tests.py
+++ python_tdd/functional_tests/tests.py
@@ -1,15 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import unittest
 
+import sys
 
-# browser = webdriver.Chrome()
-# browser.get('http://localhost:8000')
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title
 
 # 상속을 통한 Test class 생성
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     # Test 전 호출
     def setUp(self):
@@ -29,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     # test~로 명명한 test Method
     def test_can_start_a_list(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시
         self.assertIn('To-Do lists', self.browser.title)
@@ -73,7 +84,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome()
 
         # 새로운 사용자의 접속, 전 사용자의 정보는 보이지 않음
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공구 사기', page_text)
         self.assertNotIn('공구를 이용하여 조립', page_text)
@@ -100,7 +111,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # ...
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 입력상자의 가운데 배치 확인
