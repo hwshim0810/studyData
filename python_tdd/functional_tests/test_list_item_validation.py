455d61bdaa3534885e052bb5cb2ac2b3302f6d1a
Shim.hw <shim.hw0810@gmail.com>
2017-05-06 21:04:26 +0900

    Add item validation test contents

--- python_tdd/functional_tests/test_list_item_validation.py
+++ python_tdd/functional_tests/test_list_item_validation.py
@@ -4,6 +4,27 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # 빈 작업을 등록하려고 엔터키를 눌러 등록
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # 페이지 새로 고침 후, 빈 아이템은 등록불가 에러메세지 출력
-        self.fail('write')
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "빈 아이템은 등록불가")
+        
+        # 다른 아이템 입력
+        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n')
+        self.check_for_row_in_list_table('1: 우유 사기')
+        
+        # 다시 빈 아이템 등록
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        
+        # 다시 에러메세지 표시
+        self.check_for_row_in_list_table('1: 우유 사기')
+        err = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(err.text, "빈 아이템은 등록불가")
+        
+        # 내용이 있는 아이템을 등록하면 동작
+        self.browser.find_element_by_id('id_new_item').send_keys('커피 우유 만들기\n')
+        self.check_for_row_in_list_table('1: 우유 사기')
+        self.check_for_row_in_list_table('2: 커피 우유 만들기')
+
